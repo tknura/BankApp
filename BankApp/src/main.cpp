fdba5c38711750481701bb8e1648790851af9bfa
Tomasz Knura <tknura@gmail.com>
2020-03-24 19:15:22 +0100

    Updated main.cpp

--- BankApp/src/main.cpp
+++ BankApp/src/main.cpp
@@ -1,24 +1,12 @@
-<<<<<<< HEAD:BankApp/main.cpp
-#include "mainwindow.h"
-#include "Config.h"
-=======
 #include "controller\MainWindow.h"
->>>>>>> 61c719dcdaa1a8d55f88597da010f3dca956e01f:BankApp/src/main.cpp
 
-#include <iostream>
+#include "model\Bank.h"
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    try
-    {
-         Config test;
-    }
-    catch (std::ifstream::failure &e)
-    {
-        std::cout<<e.what();
-    }
-    return 0;
+   // Bank::currentlyLoggedUser = new User(1);
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
