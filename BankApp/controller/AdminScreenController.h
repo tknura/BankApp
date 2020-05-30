9a902f0105fa4ab3a756e2139cecaa412ec6c7ab
Tomasz Knura <tknura@gmail.com>
2020-05-30 15:24:33 +0200

    previous commit fix

--- BankApp/controller/AdminScreenController.h
+++ BankApp/controller/AdminScreenController.h
@@ -2,14 +2,12 @@
 #define ADMINSCREENCONTROLLER_H
 
 #include <QObject>
-#include <QQmlContext>
-#include <QStringListModel>
-#include <iostream>
 #include "IController.h"
 #include "model/Admin.h"
-#include "model/enums/AccountType.h"
 #include "AccountAdditionController.h"
 #include "CardAdditionController.h"
+#include "UserAdditionController.h"
+#include "FundAdditionController.h"
 
 class AdminScreenController : QObject, IController
 {
@@ -17,17 +15,16 @@ class AdminScreenController : QObject, IController
 
 private:
     QObject *rootObject;
+    UserAdditionController* usrAddTabController;
     AccountAdditionController* accAddTabController;
     CardAdditionController* cardAddTabController;
+    FundAdditionController* fundAddTabController;
 
 public:
     void Initialize(QQmlApplicationEngine* p_engine) override;
     AdminScreenController(QQmlApplicationEngine* p_engine);
     ~AdminScreenController() override;
     static QStringList UserList();
-
-public slots:
-    void HandleUserAddition(QString, QString, QString);
 };
 
 #endif // ADMINSCREENCONTROLLER_H
