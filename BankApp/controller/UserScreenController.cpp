ad3aea8a085d04ddc23bcd43a3d026870b7712d1
Tomasz Knura <tknura@gmail.com>
2020-06-10 13:13:08 +0200

    merge fix

--- BankApp/controller/UserScreenController.cpp
+++ BankApp/controller/UserScreenController.cpp
@@ -1,16 +1,20 @@
 #include "UserScreenController.h"
-#include "HistoryList.h"
 #include <QQmlContext>
 
 
-void UserScreenController::Initialize(QQmlApplicationEngine *p_engine) {
-    this->rootObject = p_engine->rootObjects().first()->findChild<QObject*>("adminScreen");
+void UserScreenController::Initialize() {
+
 //    HistoryList historyList;
 //    p_engine->rootContext()->setContextProperty(QStringLiteral("historyList"), &historyList);
    // rootObject->findChild<QObject*>("history")->setProperty("historylist",QVariant::fromValue(&historyList));
 
 }
 
+void UserScreenController::Connections() {
+
+}
+
 UserScreenController::UserScreenController(QQmlApplicationEngine *p_engine) {
-    Initialize(p_engine);
+    this->rootObject = p_engine->rootObjects().first()->findChild<QObject*>("adminScreen");
+    Initialize();
 }
